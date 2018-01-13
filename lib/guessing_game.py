591a1851fc8a5566522f0ef1d1bd1a20b6567874
kylesallette <kylesallette@gmail.com>
2018-01-13 11:37:59 -0700

    indenting

--- lib/guessing_game.py
+++ lib/guessing_game.py
@@ -4,12 +4,12 @@ guess = int(raw_input("Enter an integer from 1 to 99:"))
 while n != "guess":
     print
     if guess < n:
-      print "guess is to low"
-      guess = int(raw_input("Enter an integer from 1 to 99:"))
+        print "guess is to low"
+        guess = int(raw_input("Enter an integer from 1 to 99:"))
     elif guess > n:
-      print "guess is to high"
-      guess = int(raw_input("Enter an interher from 1 to 99:"))
+        print "guess is to high"
+        guess = int(raw_input("Enter an interher from 1 to 99:"))
     else:
-      print "Good job!! You guessed it!"
-      break
+        print "Good job!! You guessed it!"
+        break
     print
